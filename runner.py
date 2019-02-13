17ba44b3167477f3eee7dd5c039553a7690dd08c
Karissa Brickey <46540457+karissabrickey@users.noreply.github.com>
2019-02-13 14:03:05 -0600

    Update runner.py to work with gutenberg.py

--- runner.py
+++ runner.py
@@ -1,11 +1,16 @@
 import os
 
-def printngrams(a):
-    command = "./a.out clean_file.txt " + str(a)
-    os.system(command)
+def printngrams(num, filename):
+    command = "./a.out %s " % filename 
+    os.system(command + str(num))
+
+    # command = "./a.out clean_10001.txt " + str(num)
+    # os.system(command)
 
 
 
-if __name__ == "__main__":
-    a = int(input())
-    printngrams(a)
+# if __name__ == "__main__":
+#     num = 2 #int(input())
+#     temp = "temp"
+#     printngrams(num, temp)
+
