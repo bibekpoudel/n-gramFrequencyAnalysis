d2ac2d33ee04a52e4fc3f4260b323af2bf9b28cc
karissa <karissab02@gmail.com>
2019-02-13 17:03:00 -0500

    error

---
+++ printngrams.py
@@ -0,0 +1,17 @@
+import os
+
+def printngrams(num, filename):
+    command = "./a.out %s " % filename 
+    os.system(command + str(num))
+
+    # command = "./a.out clean_10001.txt " + str(num)
+    # os.system(command)
+
+
+
+# if __name__ == "__main__":
+#     num = 2 #int(input())
+#     temp = "temp"
+#     printngrams(num, temp)
+
+    
